e6a683acb1f3378b7d4225bd6b1080dfb7f67161
1571221458@qq.com <majiafei>
2018-12-08 18:22:35 +0800

    初次提交

---
+++ src/main/java/com/huayue/house/redis/HouseRedisApplication.java
@@ -0,0 +1,12 @@
+package com.huayue.house.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseRedisApplication.class, args);
+    }
+}

8a0068ab7b28f6f10a6af396e179116e4ef08c39
1571221458@qq.com <majiafei>
2018-12-08 18:36:23 +0800

    初次提交

--- src/main/java/com/huayue/house/redis/HouseRedisApplication.java
+++ src/main/java/com/huayue/house/redis/HouseRedisApplication.java
@@ -2,11 +2,22 @@ package com.huayue.house.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 public class HouseRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HouseRedisApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(HouseRedisApplication.class, args);
+
+        RedisTemplate redisTemplate = context.getBean(StringRedisTemplate.class);
+        redisTemplate.opsForValue().set("name", "xiaoming");
+
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println(name);
+
+        context.close();
     }
 }
